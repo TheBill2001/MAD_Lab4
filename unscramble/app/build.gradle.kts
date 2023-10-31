22ea789270216b3b525dfa5861c7c77a51222e53
Trần Nam Tuấn <Trn Nam Tun>
2023-10-31 17:04:35 +0700

    Lesson 6: App navigation - 1 Architecture Components - 3 Write unit tests for ViewModel

--- unscramble/app/build.gradle.kts
+++ unscramble/app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.06.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
@@ -80,4 +80,9 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
